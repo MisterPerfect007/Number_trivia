7bd3818a30d1f5853d4c5d74a4d745281ab42a78
MisterPerfect007 <leneodepart@gmail.com>
2022-04-11 17:47:57 +0000

    Testing the local data source implementation

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:number_trivia_app/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/number_trivia_model.dart';
+
+const String triviaKey = 'CACHED_TRIVIA_KEY';
 
 abstract class NumberTriviaLocalDataSource{
   /// Gets the cached [NumberTriviaModel] which was gotten the last time the user had internet connection.
@@ -8,4 +15,26 @@ abstract class NumberTriviaLocalDataSource{
 
   /// Caches the last gotten [NumberTriviaModel]
   Future<void> cacheLastNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+  @override
+  Future<void> cacheLastNumberTrivia(NumberTriviaModel triviaToCache) async {
+    await sharedPreferences.setString(triviaKey, jsonEncode(triviaToCache.toJson()));
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() async {
+    final String? triviaString = sharedPreferences.getString(triviaKey);
+    if(triviaString != null){
+      final triviaJson = jsonDecode(triviaString);
+      return NumberTriviaModel.fromJson(triviaJson);
+    }else {
+      throw CacheException();
+    }
+  }
+  
 }
--- test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -1,8 +1,59 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia_app/error/exceptions.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-@GenerateMocks([SharedPreferences])
+import 'number_trivia_local_data_source_test.mocks.dart';
 
-void main(){
+@GenerateMocks([SharedPreferences])
+void main() {
+  late MockSharedPreferences sharedPreferences;
+  late NumberTriviaLocalDataSourceImpl localDataSource;
+
+  setUp(() async {
+    sharedPreferences = MockSharedPreferences();
+    localDataSource =
+        NumberTriviaLocalDataSourceImpl(sharedPreferences: sharedPreferences);
+  });
+  const NumberTriviaModel numberTriviaModel =
+      NumberTriviaModel(text: 'test', number: 1);
+  final String triviaString = jsonEncode(numberTriviaModel.toJson());
+
+  test('Should cache data when cacheLastNumberTrivia will be call', () async {
+    when(sharedPreferences.setString(
+            triviaKey, triviaString))
+        .thenAnswer((_) async => true);
+
+    await localDataSource.cacheLastNumberTrivia(numberTriviaModel);
+
+    verify(sharedPreferences.setString(
+        triviaKey, triviaString));
+    verifyNoMoreInteractions(sharedPreferences);
+  });
+  test(
+      'Should return a valid NumberTriviaModel when a cached trivia is present',
+      () async {
+    when(sharedPreferences.getString(any)).thenReturn(triviaString);
+
+    final result = await localDataSource.getLastNumberTrivia();
+
+    expect(result, numberTriviaModel);
+    verify(sharedPreferences.getString(triviaKey));
+    verifyNoMoreInteractions(sharedPreferences);
+  });
+  test('Should return a CacheException when there no cached trivia present',
+      () async {
+    when(sharedPreferences.getString(triviaKey)).thenReturn(null);
+
+    final call = localDataSource.getLastNumberTrivia;
+
+    expect(() => call(), throwsA(isA<CacheException>()));
+    verify(sharedPreferences.getString(triviaKey));
+    verifyNoMoreInteractions(sharedPreferences);
+  });
 }
